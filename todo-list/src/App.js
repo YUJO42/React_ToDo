cffb5c65ff616c581fa74afd73ef8b7705048bd1
yujo <yujo@student.42seoul.kr>
2020-07-05 10:19:07 +0900

    done

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -33,7 +33,6 @@ class App extends Component {
       }),
     });
   };
-
   handleKeyPress = (e) => {
     // 눌려진 키가 Enter 면 handleCreate 호출
     if (e.key === "Enter") {
@@ -41,54 +40,29 @@ class App extends Component {
     }
   };
 
-  // handleToggle = (id) => {
-  //   const { todos } = this.state;
-
-  //   const index = todos.findIndex((todo) => todo.id === id);
-  //   const selected = todos[index];
-
-  //   const nextTodos = [...todos];
-
-  //   nextTodos[index] = {
-  //     ...selected,
-  //     checked: !selected.checked,
-  //   };
-  // };
-
   handleToggle = (id) => {
     const { todos } = this.state;
+
+    // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
     const index = todos.findIndex((todo) => todo.id === id);
+    const selected = todos[index]; // 선택한 객체
 
-    const selected = todos[index];
+    const nextTodos = [...todos]; // 배열을 복사
+
+    // 기존의 값들을 복사하고, checked 값을 덮어쓰기
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked,
+    };
 
     this.setState({
-      todos: [
-        ...todos.slice(0, index),
-        {
-          ...selected,
-          checked: !selected.checked,
-        },
-        ...todos.slice(index + 1, todos.length),
-      ],
-    });
-  };
-
-  handleRemove = (id) => {
-    const { todos } = this.state;
-    this.setState({
-      todos: todos.filter((todo) => todo.id !== id),
+      todos: nextTodos,
     });
   };
 
   render() {
     const { input, todos } = this.state;
-    const {
-      handleChange,
-      handleCreate,
-      handleKeyPress,
-      handleToggle,
-      handleRemove,
-    } = this;
+    const { handleChange, handleCreate, handleKeyPress, handleToggle } = this;
 
     return (
       <TodoListTemplate
@@ -101,13 +75,10 @@ class App extends Component {
           />
         }
       >
-        <TodoItemList
-          todos={todos}
-          onToggle={handleToggle}
-          onRemove={handleRemove}
-        />
+        <TodoItemList todos={todos} oToggle={handleToggle} />
       </TodoListTemplate>
     );
   }
 }
+
 export default App;
